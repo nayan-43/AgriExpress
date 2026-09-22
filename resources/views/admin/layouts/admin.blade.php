a1e82d9e1095a0f52c324fec9c2f36c1cce5919b
nayan-43 <mailnayan@webgrity.net>
2026-09-22 17:51:46 +0530

    update favicon

--- resources/views/admin/layouts/admin.blade.php
+++ resources/views/admin/layouts/admin.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Dashboard') &middot; AgriExpress</title>
+    <link href="{{ asset('assets/images/favicon.png') }}" rel="icon" />
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/all.min.js" crossorigin="anonymous"></script>
     @stack('head')
--- resources/views/admin/layouts/guest.blade.php
+++ resources/views/admin/layouts/guest.blade.php
@@ -12,8 +12,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Sign in') &middot; AgriExpress</title>
+    <link href="{{ asset('assets/images/favicon.png') }}" rel="icon" />
 
-    <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/all.min.js" crossorigin="anonymous"></script>
     @stack('head')
 
